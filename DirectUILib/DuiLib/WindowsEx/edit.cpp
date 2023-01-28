94c2ff46bc9102839765d9cf474fef257e70056e
KnIfER <302772670@qq.com>
2023-01-28 17:07:32 +0800

    FIXME edit.cpp :  Critical error detected c0000374
    Critical error detected c0000374 when deleting '/' from text http://127.0.0.1:8080/

--- DirectUILib/DuiLib/WindowsEx/edit.cpp
+++ DirectUILib/DuiLib/WindowsEx/edit.cpp
@@ -241,6 +241,7 @@ inline void EDIT_InvalidateUniscribeData(EDITSTATE *es)
 	}
 	if (es->ssa)
 	{
+		// Critical error detected c0000374 when deleting '/' from 'http://127.0.0.1:8080/'
 		ScriptStringFree(&es->ssa);
 		es->ssa = NULL;
 	}
