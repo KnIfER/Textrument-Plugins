55c9b1a211147e1d84f076c1b2a024fff9298923
KnIfER <302772670@qq.com>
2023-03-25 01:15:11 +0800

    bin

---
+++ DirectUILib/SkiaTests/SkImageGrid.cpp
@@ -0,0 +1,90 @@
+/****** SKia Decode PNG Region Test *********
+* 
+* ImageView. Pro And Lite. 
+* 
+**************************************/
+#include "..\DuiLib\UIlib.h"
+//#include "..\DuiLib\ControlPro\ImageViewPro.h"
+#include "..\DuiLib\ControlEx\SkImageView.h"
+#include "../DuiLib/Core/InsituDebug.h"
+using namespace DuiLib;
+
+namespace SK_IMG_VIEW {
+    class ImageViewMainForm : public WindowImplBase, public INotifyUI
+    {
+    public:
+        ImageViewMainForm() {
+            _frameLess = false;
+        };     
+        LPCTSTR GetWindowClassName() const override
+        { 
+            return _T("ImageViewMainForm"); 
+        }
+        UINT GetClassStyle() const override
+        { 
+            return CS_DBLCLKS; 
+        }
+        void OnFinalMessage(HWND hWnd) override
+        { 
+            __super::OnFinalMessage(hWnd);
+            delete this;
+        }
+        LRESULT OnClose(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& bHandled) override
+        {
+            ::DestroyWindow(GetHWND());
+            bHandled = TRUE;
+            return 0;
+        }
+        void InitWindow() override
+        {
+            ivTest = dynamic_cast<ImageView*>(m_pm.FindControl(L"ivTest"));
+            if (ivTest)
+            {
+                 //ivTest->LoadImageFile("D:\\test.png");
+                ivTest->LoadImageFile("D:\\Large-Sample-Image-download-for-Testing.webp");
+               // ivTest->LoadImageFile("D:\\Large-Sample-Image-download-for-Testing.jpg");
+                //ivTest->LoadImageFile("C:\\Users\\TEST\\Pictures\\MTkzMDI1NDI2XzExNTk2Njg3MDIx_0.webp");
+            }
+        }
+        QkString GetSkinFile() override
+        {
+            return _T("testImageGrid.xml");
+        }
+
+        void Notify( TNotifyUI &msg ) override
+        {
+            if (msg.sType==L"click")
+            {
+            }
+            WindowImplBase::Notify(msg);
+        }
+
+    private:
+        ImageView* ivTest;
+    };
+
+    QkString Name = L"五、SkImageViewTest";
+
+    LRESULT RunTest(HINSTANCE hInstance, HWND hParent)
+    {
+        if (hInstance==NULL) return (LRESULT)Name.GetData();
+        CPaintManagerUI::SetInstance(hInstance);
+        CPaintManagerUI::SetResourcePath(CPaintManagerUI::GetInstancePath());
+
+        CControlFactory::GetInstance()->RegistControl(TEXT("ImageView"), ImageView::CreateControl);
+        //CControlFactory::GetInstance()->RegistControl(TEXT("ImageView"), SkImageView::CreateControl);
+
+        HRESULT Hr = ::CoInitialize(NULL);
+        if( FAILED(Hr) ) return 0;
+
+        ImageViewMainForm* pFrame = new ImageViewMainForm();
+        if( pFrame == NULL ) return 0;
+        pFrame->Create(NULL, _T("Skia ImageView For The DuiLib !!!"), UI_WNDSTYLE_FRAME|WS_CLIPCHILDREN, WS_EX_WINDOWEDGE);
+        pFrame->CenterWindow();
+        pFrame->ShowWindow(true);
+        return 0;
+    }
+
+    static int _auto_reg = AutoRegister(RunTest);
+
+}
